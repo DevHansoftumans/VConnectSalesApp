13a78be785bdb3c06c715e11d7cfb7e01b431627
Karthik Ramachandran <karthikr1493@gmail.com>
2022-04-16 20:10:54 +0530

    Added Working Status screen with Radio Button.

---
+++ app/src/main/java/com/vconnectapp/ui/gallery/MyStatusFragment.kt
@@ -0,0 +1,68 @@
+package com.vconnectapp.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.vconnectapp.R
+import com.vconnectapp.databinding.FragmentMyStatusBinding
+
+class MyStatusFragment : Fragment() {
+
+    private var _binding: FragmentMyStatusBinding? = null
+    private lateinit var myStatusViewModel: MyStatusViewModel
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding
+        get() = _binding!!
+    private var myStatus: Boolean? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMyStatusBinding.inflate(inflater, container, false)
+        myStatusViewModel =
+            ViewModelProvider(this).get(MyStatusViewModel::class.java)
+        val root: View = binding.root
+
+        initRadioGroupListeners()
+
+        return root
+    }
+
+    private fun initRadioGroupListeners() {
+        with(binding) {
+            workingStatusRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+                run {
+                    myStatus = when (checkedId) {
+                        R.id.working_radio_button -> true
+
+                        R.id.not_working_radio_button -> false
+
+                        else -> null
+                    }
+                    this.saveStatusButton.isEnabled = true
+                }
+
+            }
+
+            saveStatusButton.apply {
+                setOnClickListener {
+                    if (isEnabled) {
+                        myStatusViewModel.saveMyStatus(myStatus)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/vconnectapp/ui/gallery/MyStatusViewModel.kt
@@ -0,0 +1,10 @@
+package com.vconnectapp.ui.gallery
+
+import androidx.lifecycle.ViewModel
+
+class MyStatusViewModel : ViewModel() {
+
+    fun saveMyStatus(myStatus: Boolean?) {
+        //TODO Save Working status to Firebase.
+    }
+}
