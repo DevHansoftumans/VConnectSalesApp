27cdeec386fdc9ec72c8a5671002ca1d5e3fddbf
Karthik Ramachandran <karthikr1493@gmail.com>
2022-04-30 22:24:40 +0530

    Unresolved References fix

---
+++ app/src/main/java/com/vconnectapp/ui/mystatus/MyStatusViewModel.kt
@@ -0,0 +1,10 @@
+package com.vconnectapp.ui.mystatus
+
+import androidx.lifecycle.ViewModel
+
+class MyStatusViewModel : ViewModel() {
+
+    fun saveMyStatus(myStatus: Boolean?) {
+        //TODO Save Working status to Firebase.
+    }
+}
---
+++ app/src/main/java/com/vconnectapp/utils/CheckpointsUtility.kt
@@ -0,0 +1,7 @@
+package com.vconnectapp.utils
+
+object CheckpointsUtility {
+        const val CHECKPOINT_1 = 1;
+        const val CHECKPOINT_2 = 2;
+        const val CHECKPOINT_3 = 3;
+}
